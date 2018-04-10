1efb0c606d5ecbbc0bf35fce3de5544abc0cf1dc
Miki <miki_wild@hotmail.com>
2018-04-10 11:40:30 -0400

    Add all production code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:edit,:update, :show, :destory]
   
-  def infex
+  def index
     @articles = Article.all
   end
+  
   def new
-    @article = Article.new(article_params)
+    @article = Article.new
     if @article.save
       flash[:notice] = "article was successfully created"
       redirect_to article_path(@article)
@@ -16,19 +18,18 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    redirec_to article_path(@article)
+    redirect_to article_path(@article)
   end
   
   def show 
-    @article = Article.find(params[:id])
+   
   end
   
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -38,13 +39,16 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to article_path
   end
   
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
   def article_params
     params.require(:article).permit(:title, :description)
   end
