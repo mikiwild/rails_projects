5c4e49f19692cf2f184b10707a1abe84dca2195b
Miki <miki_wild@hotmail.com>
2018-04-11 13:51:52 -0400

    Styling error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only:[:edit,:update, :show, :destory]
+  before_action :set_article, only:[:edit,:update, :show, :destroy]
   
   def index
     @articles = Article.all
@@ -7,41 +7,37 @@ class ArticlesController < ApplicationController
   
   def new
     @article = Article.new
+  end
+  
+  def edit
+  end
+  
+  def create
+    @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "article was successfully created"
+      flash[:success] = "article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end
   
-  def create
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
-  end
-  
-  def show 
-   
-  end
-  
-  def edit
-    
-  end
-  
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render'edit'
     end
   end
   
+  def show 
+  end
+  
   def destroy
-    @article.destroy
-    flash[:notice] = "Article was successfully deleted"
-    redirect_to article_path
+      @article.destroy
+     flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
   end
   
   private
