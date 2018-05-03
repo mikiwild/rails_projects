38495cda8e7dcb5d14f1d655c7116d3c48fde71a
Miki <miki_wild@hotmail.com>
2018-05-03 14:36:36 -0400

    Modify for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,5 +50,6 @@ class UsersController < ApplicationController
     if current_user != @user
       flash[:danger] = "You can only edit your own accont"
       redirect_to root_path
+    end
   end
 end
