44b38f8a24e644954e36c3adf099f80a36c69b28
Miki <miki_wild@hotmail.com>
2018-05-03 14:29:23 -0400

    Add protect area

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'pages/home', to: 'pages#home'
+ # get 'pages/home', to: 'pages#home'
   get 'about', to: 'pages#about'
   
   resources :articles
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
+  
+  
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
+   delete 'logout', to: 'sessions#destroy'
+ 
+  #resources :sessions
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
