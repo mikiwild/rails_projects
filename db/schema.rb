1a88304b6029595bd174683e01b60157ba111316
Miki <miki_wild@hotmail.com>
2018-05-10 11:46:13 -0400

    Add article category table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508140647) do
+ActiveRecord::Schema.define(version: 20180510145741) do
+
+  create_table "article_categories", force: true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: true do |t|
     t.string   "title"
